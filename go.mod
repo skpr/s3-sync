11b4f717697e7b6da33150316750682f2b5689e9
Nick Schuch <nick@previousnext.com.au>
2022-02-04 09:22:39 +1000

    Adds exclude functionality

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/skpr/s3-sync
 
 go 1.17
 
+replace github.com/seqsense/s3sync => github.com/skpr/s3sync v1.8.2-0.20220203223008-e72bb50af2a9
+
 require (
 	github.com/aws/aws-sdk-go v1.42.45
 	github.com/seqsense/s3sync v1.8.1
@@ -12,6 +14,7 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -11,7 +13,9 @@ import (
 var (
 	cliRegion   = kingpin.Flag("region", "When transferring objects from an s3 bucket to an s3 bucket, this specifies the region of the source bucket").Default(endpoints.ApSoutheast2RegionID).String()
 	cliEndpoint = kingpin.Flag("endpoint", "Override command's default URL with the given URL").String()
-	cliParallel = kingpin.Flag("parallel", "Sets maximum number of parallel file sync jobs.").Default("4").Int()
+	cliParallel = kingpin.Flag("parallel", "Sets maximum number of parallel file sync jobs").Default("16").Int()
+	cliDelete   = kingpin.Flag("delete", "Delete files which are not listed in the source").Bool()
+	cliExclude  = kingpin.Flag("exclude", "Exclude paths from the list to be synced").Envar("SKPR_S3_SYNC_EXCLUDE").String()
 	cliSource   = kingpin.Arg("source", "Source files which are synced (local or S3 path)").Required().String()
 	cliTarget   = kingpin.Arg("target", "Target files which are synced (local or S3 path)").Required().String()
 )
@@ -29,7 +33,21 @@ func main() {
 		panic(err)
 	}
 
-	syncManager := s3sync.New(sess, s3sync.WithParallel(*cliParallel), s3sync.WithDelete())
+	var options []s3sync.Option
+
+	if *cliDelete {
+		options = append(options, s3sync.WithDelete())
+	}
+
+	if *cliParallel > 0 {
+		options = append(options, s3sync.WithParallel(*cliParallel))
+	}
+
+	for _, exclude := range strings.Split(*cliExclude, ",") {
+		options = append(options, s3sync.WithExcludePattern(exclude))
+	}
+
+	syncManager := s3sync.New(sess, options...)
 
 	err = syncManager.Sync(*cliSource, *cliTarget)
 	if err != nil {
