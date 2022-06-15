db8d3d9e21eb73dfab415b0e262ac0d05101bf74
Nick Schuch <nick@previousnext.com.au>
2022-06-15 20:13:44 +1000

    Adds htaccess exclude by default

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var (
 	cliRegion   = kingpin.Flag("region", "When transferring objects from an s3 bucket to an s3 bucket, this specifies the region of the source bucket").Default("ap-southeast-2").String()
 	cliEndpoint = kingpin.Flag("endpoint", "Override command's default URL with the given URL").Envar("SKPR_S3_SYNC_ENDPOINT").String()
 	cliDelete   = kingpin.Flag("delete", "Delete files which are not listed in the source").Envar("SKPR_S3_SYNC_DELETE").Bool()
-	cliExclude  = kingpin.Flag("exclude", "Exclude paths from the list to be synced").Envar("SKPR_S3_SYNC_EXCLUDE").String()
+	cliExclude  = kingpin.Flag("exclude", "Exclude paths from the list to be synced").Envar("SKPR_S3_SYNC_EXCLUDE").Default(".htaccess").String()
 	cliSource   = kingpin.Arg("source", "Source files which are synced (local or S3 path)").Required().String()
 	cliTarget   = kingpin.Arg("target", "Target files which are synced (local or S3 path)").Required().String()
 )
