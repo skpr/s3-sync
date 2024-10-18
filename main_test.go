1ac648d19b1406357f84b819ab72d80bb95a981d
Nick Schuch <nick@previousnext.com.au>
2024-10-18 13:44:23 +1000

    Adds mode

--- main_test.go
+++ main_test.go
@@ -7,16 +7,21 @@ import (
 )
 
 func TestBuildArgsWithEndpoint(t *testing.T) {
-	command := buildArgs("127.0.0.1", "foo", "bar", "")
+	command := buildArgs("127.0.0.1", "sync", "foo", "bar", "")
 	assert.Equal(t, []string{"s3", "--endpoint-url", "127.0.0.1", "sync", "foo", "bar"}, command)
 }
 
+func TestBuildArgsWithEndpointAndCP(t *testing.T) {
+	command := buildArgs("127.0.0.1", "cp", "foo", "bar", "")
+	assert.Equal(t, []string{"s3", "--endpoint-url", "127.0.0.1", "cp", "foo", "bar"}, command)
+}
+
 func TestBuildArgsdWithExcludes(t *testing.T) {
-	command := buildArgs("", "foo", "bar", "/stuff,/things")
+	command := buildArgs("", "sync", "foo", "bar", "/stuff,/things")
 	assert.Equal(t, []string{"s3", "sync", "--exclude", "/stuff", "--exclude", "/things", "foo", "bar"}, command)
 }
 
 func TestBuildArgsWithAll(t *testing.T) {
-	command := buildArgs("127.0.0.1", "foo", "bar", "/stuff,/things")
+	command := buildArgs("127.0.0.1", "sync", "foo", "bar", "/stuff,/things")
 	assert.Equal(t, []string{"s3", "--endpoint-url", "127.0.0.1", "sync", "--exclude", "/stuff", "--exclude", "/things", "foo", "bar"}, command)
 }
